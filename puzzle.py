748021f2ee970d5ea887afb9fe9de83dfea8e317
Ayushverma408 <ayushverma462002@gmail.com>
2021-01-25 21:36:37 +0530

    Update puzzle.py

--- puzzle.py
+++ puzzle.py
@@ -2,14 +2,14 @@
 import random
 
 #defining the input list
-input_list = ["narcissist", "selfobsessed", "inferiority", "ideals", "king", "fake", "Crumble"]
+input_list = ["n a r c i s s i s t ", "s e l f o b s e s s e d ", "i n f e r i o r i t y ", "i d e a l s ", "k i n g ", "f a k e ", "c r u m b l e "]
 
 bucket_1 = [1, 2, 3, 8, 9, 10, 11, 12, 13, 14, 15]
 
 for i in range(7):
     print(input_list[i], end="")
-    left_letters = 27 - len(input_list[i])
+    left_letters = 27 - len(input_list[i].replace(" ",""))
     for j in range(left_letters):
-        print(chr(random.randrange(97, 97 + 26)), end="")
+        print(chr(random.randrange(97, 97 + 26)), end=" ")
     print(" ")
     
